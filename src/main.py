f2ec4228c4b5eed3d910486ca946026a85714e87
Antoine <antoine.sb@orange.fr>
2019-02-28 18:27:38 +0000

    add slide{} and slideshow{}

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
+class slide:
+	data = {}
+	orientation = {}
+	def __init__(_orientation, _data) -> bool:
+		"""
+		if (self.orientation == 'V' and len(self.data) != 2)
+		or (self.orientation == 'H' and len(self.data) != 1):
+			return False
+		"""
+		self.orientation = _orientation
+		self.data = _data
 
+class slideshow:
+	data = list() # list of slides
+	def __init__(first_slide: slide):
+		data.append(photo)
 
 def main():
 	"""Script entry point"""
