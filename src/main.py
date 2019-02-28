6556416b160956296725147bafc1b04ace706478
Antoine <antoine.sb@orange.fr>
2019-02-28 19:12:38 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,32 +6,56 @@ class orientation(Enum):
 	H = 'H'
 	V = 'V'
 
+class photo:
+	id = 0
+	orientation = {}
+	tags = []
+	used = False
+
+	def __init__(self, _id: int, orien: orientation, tag: str):
+		self.id = _id;
+		self.orientation = orien
+		self.tags = tag
+
+class photos_list:
+	data = list()
+	def __init__(self, first_photo: photo):
+		self.data.append(first_photo)
+
 class slide:
 	data = {} # one or two photos
-	orientation = {} # 'H' or 'V'
+	orientation = {}
 	tags = set() # set of strings
-	def __init__(_orientation, _data) -> bool:
-		if (_orientation == 'V' and len(_data) != 2) or (_orientation == 'H' and len(_data) != 1):
+	def __init__(self, _orientation: orientation, _data) -> bool:
+		if (_orientation == V and len(_data) != 2) or (_orientation == H and len(_data) != 1):
 			return False
 		self.orientation = _orientation
 		self.data = _data
 
 class slideshow:
 	data = list() # list of slides
-	def __init__(first_slide: slide):
-		data.append(photo)
+	def __init__(self, first_slide: slide):
+		self.data.append(first_slide)
 
-def parse(filename: str) -> slideshow:
+def parse(filename: str) -> photos_list:
 	datasets_folder = "../datasets/"
 	file = open(datasets_folder + filename)
-	file.readline()
-	for line in file:
-		print(line[:-1])
 
+	def parse_line(line: str) -> photo:
+		print(line[:-1].split(' '))
+		return {}
+
+	file.readline()
+	_photos_list = photos_list(parse_line(file.readline()))
+
+	for line in file:
+		parse_line(line)
+
+	return _photos_list
 
 def main():
 	"""Script entry point"""
-	parse("a_example.txt")
+	photos = parse("a_example.txt")
 
 if __name__ == "__main__":
 	main()
